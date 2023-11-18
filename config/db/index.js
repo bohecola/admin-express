f63398fd5550fa51242e5cb106cf48fc88b2c262
bohecola <bohecola@outlook.com>
2023-11-18 21:37:59 +0800

    chore: update db config

--- config/db/index.js
+++ config/db/index.js
@@ -1,7 +1,7 @@
 module.exports = {
   // 数据库配置
   DB_SERVER: 'localhost',
-  DB_NAME: 'admin-demo',
-  DB_USER: 'admin-demo',
+  DB_NAME: 'admin-express',
+  DB_USER: 'bohe',
   DB_SECRET: '123456'
 }
